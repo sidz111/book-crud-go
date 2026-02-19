fbf76f9eabf167c76150de188069394064288611
sidz111 <sssurwade2212@gmail.com>
2026-02-19 10:50:18 +0530

    model/Book Created

---
+++ internal/model/book.go
@@ -0,0 +1,17 @@
+package main
+
+type Book struct {
+	ID            int    `json:"id,omitempty"`
+	Name          string `json:"name,omitempty"`
+	AuthorName    string `json:"author_name,omitempty"`
+	PublishedYear int    `json:"published_year,omitempty"`
+}
+
+func New(id int, name string, authorName string, publishedYear int) *Book {
+	return &Book{
+		ID:            id,
+		Name:          name,
+		AuthorName:    authorName,
+		PublishedYear: publishedYear,
+	}
+}
