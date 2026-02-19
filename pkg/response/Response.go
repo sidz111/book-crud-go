d7e40b09c8a27b2c3740288b8872840775ed9654
sidz111 <sssurwade2212@gmail.com>
2026-02-19 14:59:41 +0530

    Updated Response file

--- pkg/response/Response.go
+++ pkg/response/Response.go
@@ -1,7 +1,33 @@
 package response
 
-type Response struct {
-	Status  int
-	Message string
-	Data    any
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Success response
+func JSON(w http.ResponseWriter, status int, message string, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	res := map[string]interface{}{
+		"status":  status,
+		"message": message,
+		"data":    data,
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+// Error response
+func Error(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	res := map[string]interface{}{
+		"status":  status,
+		"message": message,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
