1c0b75ce5ad4f694d68daa276fcd1b148a1f3995
sidz111 <sssurwade2212@gmail.com>
2026-02-19 11:34:03 +0530

    Worked on Service Layer

---
+++ internal/service/book_service.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"github.com/sidz111/book-crud-go/internal/model"
+	"github.com/sidz111/book-crud-go/internal/repository"
+)
+
+type BookService struct {
+	Repo *repository.BookRepository
+}
+
+func (s *BookService) Save(book model.Book) error {
+	return s.Repo.Save(book)
+}
+
+func (s *BookService) Update(book model.Book) model.Book {
+	return s.Repo.Update(book)
+}
+
+func (s *BookService) DeleteBookById(id int) (string, error) {
+	return s.Repo.DeleteById(id)
+}
+
+func (s *BookService) GetAllBooks() ([]model.Book, error) {
+	return s.Repo.GetAllBooks()
+}
+
+func (s *BookService) GetBookById(id int) model.Book {
+	return s.Repo.GetBookById(id)
+}
