c39adc424d857d4b8172ad395cfb33aff5a39c5c
sidz111 <sssurwade2212@gmail.com>
2026-02-19 10:43:12 +0530

    Worked on config/db

--- config/db.go
+++ config/db.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"database/sql"
+	"log"
+)
+
+func ConnectDB() *sql.DB {
+	db_url := "root:root@tcp(localhost:3303)/books"
+
+	db, err := sql.Open("mysql", db_url)
+
+	if err != nil {
+		log.Fatal("DB Not Found")
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Failed to connect DB")
+	}
+	return db
+}
